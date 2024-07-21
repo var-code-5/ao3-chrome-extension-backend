1761a8158d87a360a7628536c9643108bc754c55
var-code-5 <varshithisgod@gmail.com>
2024-07-21 14:37:55 +0530

    fix:reomoved the register form

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.use(bodyParser.urlencoded({extended: true}));
 
 //login routes
-router.get("/login", func.get_login); //use this when using the register file with backend
+// router.get("/login", func.get_login); //use this when using the register file with backend
 router.post("/login", func.post_login);
 router.post("/register", hashPassword ,func.post_register);
 router.get("/token/:token", func.get_token);
