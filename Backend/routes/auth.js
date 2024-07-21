8e314a027559626da81bf59e0574c6c946be749f
var-code-5 <varshithisgod@gmail.com>
2024-07-21 13:16:02 +0530

    fix:add bodyparser to auth

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,8 +1,10 @@
 import express from "express";
 import * as func from "../controllers/auth.js";
 import { hashPassword } from "../middelware/auth_middleware.js";
+import bodyParser from 'body-parser';
 
 const router = express.Router();
+router.use(bodyParser.urlencoded({extended: true}));
 
 //login routes
 // router.get("/login", func.get_login); //use this when using the register file with backend
