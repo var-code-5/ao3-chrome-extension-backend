1ded4792e4c1a27be19d3880653518f6794abf05
var-code-5 <varshithisgod@gmail.com>
2024-08-18 01:04:10 +0530

    fix: add two tesing routes

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -13,7 +13,7 @@ router.post("/login", func.post_login);
 router.post("/register", hashPassword ,func.post_register);
 router.get("/token/:token", func.get_token);
 router.post("/logout", func.post_logout);
-// router.post("/validate", func.post_verify);
-router.get("/validate", verify_token,(req,res)=>{res.status(200).send({"success":true})});
+router.post("/validate", func.post_verify);
+router.get("/validate-middle", verify_token,(req,res)=>{res.status(200).send({"success":true})});
 
 export default router;
