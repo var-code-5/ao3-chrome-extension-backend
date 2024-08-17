5521c121eb4c468b4783a6f03d131c6e8891d4e8
var-code-5 <varshithisgod@gmail.com>
2024-08-18 00:40:55 +0530

    feat: try new method by routes

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -2,6 +2,7 @@ import express from "express";
 import * as func from "../controllers/auth.js";
 import { hashPassword } from "../middelware/auth_middleware.js";
 import bodyParser from 'body-parser';
+import {verify_token} from "../middelware/api_middleware.js"
 
 const router = express.Router();
 router.use(bodyParser.urlencoded({extended: true}));
@@ -12,6 +13,7 @@ router.post("/login", func.post_login);
 router.post("/register", hashPassword ,func.post_register);
 router.get("/token/:token", func.get_token);
 router.post("/logout", func.post_logout);
-router.post("/validate", func.post_verify);
+// router.post("/validate", func.post_verify);
+router.get("/validate", verify_token,(req,res)=>{res.status(200).send({"success":true})});
 
 export default router;
