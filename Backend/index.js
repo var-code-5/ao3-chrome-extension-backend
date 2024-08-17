0e20e2c5463253e585d3efda9422547aecf58a45
var-code-5 <varshithisgod@gmail.com>
2024-08-17 22:26:25 +0530

    fix : cookie secure

--- Backend/index.js
+++ Backend/index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(cors({
   origin: "http://localhost:5173",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  allowedHeaders: "Content-Type,Authorization",
+  // allowedHeaders: "Content-Type,Authorization",
   credentials: true
 }));
 app.use(bodyParser.urlencoded({ extended: true }));
