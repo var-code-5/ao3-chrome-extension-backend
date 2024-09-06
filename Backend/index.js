46ce45d9c0a54533d11bc614f53ab866916b54fa
var-code-5 <varshithisgod@gmail.com>
2024-09-06 12:41:09 +0530

    fix: auth headers

--- Backend/index.js
+++ Backend/index.js
@@ -26,7 +26,7 @@ app.use(cors({
     }
   },
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  // allowedHeaders: "Content-Type,Authorization",
+  allowedHeaders: "Content-Type,Authorization",
   credentials: true
 }));
 
