61563c82349366d81229aea5ffe4183f87250a09
var-code-5 <varshithisgod@gmail.com>
2024-09-19 17:37:55 +0530

    feat: add chrome-extension cors

--- Backend/index.js
+++ Backend/index.js
@@ -3,8 +3,8 @@ import env from "dotenv";
 import auth from "./routes/auth.js";
 import aiml from "./routes/aiml.js";
 import bodyParser from "body-parser";
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 env.config();
 
@@ -12,18 +12,21 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 // middleware
-const allowedOrigins = [
-  "https://ao3-chrome-extension-website.vercel.app",
-  "https://localhost:5173" // only for dev env
+var whitelist = [
+  "https://ao3-chrome-extension-website.vercel.app/",
+  "chrome-extension://nnmmeljlhmhpnfphcpifdahblfmhlilm",
 ];
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
 
-app.use(cors({
-  origin: "https://localhost:5173",
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  allowedHeaders: "Content-Type,Authorization",
-  credentials: true
-}));
-
+app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/auth", auth);
 app.use(aiml);
@@ -33,7 +36,7 @@ app.use(cookieParser());
 //the following will be redirected to the dashboard for now to the login page
 app.get("/", (req, res) => {
   // res.redirect("/auth/login");
-  res.status(200).send({"msg":"home route"})
+  res.status(200).send({ msg: "home route" });
 });
 
 app.listen(port, () => {
